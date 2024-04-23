c388cc5f120963bcf9254cd8373aff3e363e2b9a
Trongnhat191 <nhat.nt215625@sis.hust.edu.vn>
2024-04-23 22:12:57 +0700

    end of #37

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,7 +31,7 @@ let handleLogin =async(req,res) =>{
 let handleGetAllUsers= async (req,res)=>{
     let id = req.body.id; // = all hoac id
     let users = await userService.getAllUsers(id);
-
+    console.log(users);
     return res.status(200).json({
         errCode: 0,
         errMessage: 'ok',
